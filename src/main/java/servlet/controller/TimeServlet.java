96deed1acb79e5d9cac8f8cc5a673d93e40648b7
YevhenStoliarenko <stoliarenkoeugen@gmail.com>
2024-06-10 22:35:53 +0200

    HW10 is ready

---
+++ src/main/java/servlet/controller/TimeServlet.java
@@ -0,0 +1,30 @@
+package servlet.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+@WebServlet(value = "/time/*")
+public class TimeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            String timezone = req.getParameter("timezone");
+            if (timezone.equals("")) {
+                resp.getWriter().write(TimeZome.getLocalTime("GMT+2") + " " + "GMT+2");
+            } else {
+                String replace = timezone.replace(" ", "+");
+                resp.getWriter().write(TimeZome.getLocalTime(replace) + " " + replace);
+            }
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        resp.getWriter().close();
+    }
+}
